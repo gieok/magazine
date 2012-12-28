2a087a9fa2170c2380e52ec9d91d942df0ca9b7d
yeaz <yeaz@stanford.edu>
2012-12-28 18:46:35 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gieok::Application.config.secret_token = 'e05d0df5c2de73dc5f009868df47039a1f156ef3f71448935e491dc8fc233a8ceab86c57f752d73050bb7794cb6a79014ecd5558d73b8e67bdc215d0b0181d91'
